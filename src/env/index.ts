66013958a74c19f83fc9ebf44ef19bf2e9f9880e
Bryan Zapata <bmzc95@gmail.com>
2023-09-01 23:43:46 -0500

    config eslint of rocketseat

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,19 +1,19 @@
-import "dotenv/config";
-import { z } from "zod";
+import 'dotenv/config'
+import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z
-    .enum(["development", "test", "production"])
-    .default("development"),
+    .enum(['development', 'test', 'production'])
+    .default('development'),
   PORT: z.coerce.number().default(3333),
-});
+})
 
-const _env = envSchema.safeParse(process.env); // Verify if has all variables in environment
+const _env = envSchema.safeParse(process.env) // Verify if has all variables in environment
 
 if (_env.success === false) {
-  console.error("❌ Invalid environment variables", _env.error.format());
+  console.error('❌ Invalid environment variables', _env.error.format())
 
-  throw new Error("Invalid environment variables");
+  throw new Error('Invalid environment variables')
 }
 
-export const env = _env.data;
+export const env = _env.data
